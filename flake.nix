05b9ad5879ea470ab2940f29c405a47bb56815ba
Pierre Blank <pblank@nanoxplore.com>
2024-07-23 14:16:43 +0200

    Add nix

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  inputs = {
+    naersk.url = "github:nix-community/naersk/master";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, utils, naersk }:
+    utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        naersk-lib = pkgs.callPackage naersk { };
+      in
+      {
+        defaultPackage = naersk-lib.buildPackage ./.;
+        devShell = with pkgs; mkShell rec {
+          buildInputs = [ cargo rustc rustfmt pre-commit rustPackages.clippy openssl wayland libxkbcommon vulkan-loader ];
+          nativeBuildInputs = with pkgs; [ pkg-config ];
+          RUST_SRC_PATH = rustPlatform.rustLibSrc;
+          LD_LIBRARY_PATH = "${lib.makeLibraryPath buildInputs}";
+        };
+      }
+    );
+}
